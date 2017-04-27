f98ae785c8c9952db4f3d097da075c43f774cadf
Piedutch <zae112@hotmail.com>
2017-04-27 16:25:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/stanley/fyp_assets_tracking_app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.stanley.fyp_assets_tracking_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+    }
+}
